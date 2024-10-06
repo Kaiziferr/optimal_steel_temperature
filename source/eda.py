b7f97ea699d4044024d1a37f1742415b6be3de2f
Kaiziferr <kaiziferr1995@gmail.com>
2024-10-06 17:05:40 -0500

    add functions the pca

--- source/eda.py
+++ source/eda.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.decomposition import PCA
+
 
 def porcentaje_nulos_x_caracteristica(data:pd.DataFrame, **kwargs)->None:
   """Calcular el porcentaje de valores nulos por caracteristicas"""
@@ -87,3 +89,64 @@ def calculate_time(
     except Exception as e:
         print(repr(e))
         return 0
+    
+
+def score_mi_plot(score, features):
+    """Plotting MI score"""
+    plt.hlines(y=features, xmin=0, xmax=score, color='skyblue')
+    plt.plot(score, features, "o")
+    plt.title("Mutual Information Scores", loc='left')
+    plt.xlabel('Score')
+    plt.ylabel('Feature Group')
+
+
+
+def componentes_principales(data:object,
+                            min_explained_variance:int=0.85,
+                            min_component:int=3,
+                            **kwards)->tuple:
+  """simple method of PCA, that return data transform"""
+  final_comp = 0
+  for comp in range(min_component, data.shape[1]):
+    pca = PCA(n_components=comp, **kwards)
+    pca.fit(data)
+    comp_check = pca.explained_variance_ratio_
+    final_comp = comp
+    if comp_check.sum() >= min_explained_variance:
+      break
+
+  final_pca = PCA(n_components=final_comp, **kwards)
+  final_pca.fit(data)
+  data_df = final_pca.transform(data)
+  info = "Using {} components, we can explain {}% of the variability\
+  in the original data.".format(final_comp,comp_check.sum())
+
+  return data_df, final_pca, info
+
+
+def scree_plot(
+    final_pca:object,
+    paleta,
+    size:tuple=(10,5),
+    title:str='Scree Plot'
+    )->None:
+  """Permite visualizar la varianza de cada componente"""
+  number_components = final_pca.n_components
+  principal_components = [f'PC{i+1}' for i in range(number_components)]
+  explained_variance = final_pca.explained_variance_ratio_
+  cumulative_variance = np.cumsum(explained_variance)
+  plt.figure(figsize = size)
+  plt.bar(principal_components, explained_variance, color=paleta[0])
+  plt.plot(
+      principal_components,
+      cumulative_variance,
+      'o-',
+      linewidth=2,
+      color=paleta[1])
+
+  for i,j in zip(principal_components, cumulative_variance):
+    plt.annotate(str(round(j,2)), xy=(i, j))
+  plt.title(title)
+  plt.xlabel('Principal Components')
+  plt.ylabel('Explained Variance')
+  plt.show()
